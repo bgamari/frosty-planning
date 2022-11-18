82c77eaa1b25b211f3802b0a1d9f9298503676c6
Ben Gamari <ben@smart-cactus.org>
2022-11-18 12:02:21 -0500

    Finish calendar export

--- app/ToICal.hs
+++ app/ToICal.hs
@@ -2,13 +2,18 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveAnyClass #-}
 
+-- | Converts a CSV of races into an ICal calendar.
 module Main where
 
+import Data.Time.Clock
 import Data.Time.Calendar
 import Data.Time.LocalTime
+import Data.Time.Format
+import Data.Time.Zones
 import Data.Csv as Csv
 import qualified Data.Vector as V
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Char8 as BS
 import GHC.Generics
 
 import ICal
@@ -22,15 +27,30 @@ data Row = Row
     }
     deriving (Show, Eq, Generic, Csv.FromRecord)
 
+instance FromField Day where
+    parseField =
+        return . parseTimeOrError True defaultTimeLocale "%Y-%m-%d" . BS.unpack
+
+instance FromField TimeOfDay where
+    parseField =
+        return . parseTimeOrError True defaultTimeLocale "%H:%M" . BS.unpack
+
 main :: IO ()
 main = do
-    let fname = ""
-    r <- Csv.decode NoHeader <$> BL.readFile fname
+    r <- Csv.decode NoHeader <$> BL.getContents
     rows <- either fail (return . V.toList) r
     tz <- loadSystemTZ "America/New_York"
+    let duration = secondsToNominalDiffTime $ 60*60*2
     writeFile "out.vcs" $ toVCal
         [ Event { eventSummary = "Race " ++ show (rowRace r)
-                , eventStart = localTimeToUTCTZ tz $ LocalTime ZonedTime (rowMeetingTime r) 
+                , eventStart = start
+                , eventEnd = addUTCTime duration start
+                , eventDescription = "Race " ++ show (rowRace r) ++ " at " ++ rowWhere r
+                , eventLocation = rowWhere r
                 }
         | r <- rows
+        , let start = case localTimeToUTCFull tz $ LocalTime (rowDay r) (rowMeetingTime r)  of
+                        LTUNone{}      -> error "LTUNone"
+                        LTUUnique r _  -> r
+                        LTUAmbiguous{} -> error "LTUAmbiguous"
         ]

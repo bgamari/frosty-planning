fa91a9ce203e122f8bf74c91b535c4df8276c0ce
Ben Gamari <ben@smart-cactus.org>
2023-01-28 19:33:57 -0500

    More tabulation

--- Scoring.hs
+++ Scoring.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE PatternGuards #-}
 
-module Scoring where
+module Scoring
+    ( Points(..)
+    , scoreSeries
+    ) where
 
 import Data.Foldable
 import Data.Maybe
@@ -14,39 +18,29 @@ import qualified Data.Set as S
 import Results
 
 newtype Points = Points Float
-    deriving (Show)
+    deriving (Show, Eq, Ord)
 
 instance Monoid Points where
     mempty = Points 0
 instance Semigroup Points where
     Points a <> Points b = Points (a+b)
 
-newtype Scores = Scores { getScores :: M.Map Sailor Points }
-    deriving (Show)
-
-instance Monoid Scores where
-    mempty = Scores mempty
-
-instance Semigroup Scores where
-    Scores a <> Scores b = Scores (M.unionWith (<>) a b)
-
-scoreSeries :: M.Map String [Race] -> Scores
-scoreSeries series = series'
+scoreSeries :: M.Map String [Race]
+            -> M.Map String [M.Map Sailor Points]
+scoreSeries series = fmap scoreDay series
   where
     allSailors = foldMap (foldMap raceParticipants) series
-    series' :: Scores
-    series' = foldMap scoreDay series
+    -- Build complete record of race scores, including DNFs and RC duty.
+    scoreDay :: [Race] -> [M.Map Sailor Points]
+    scoreDay races = fmap scoreRace races
       where
-        -- Build complete record of race scores, including DNFs and RC duty.
-        scoreDay :: [Race] -> Scores
-        scoreDay races = Scores $ M.fromList
+        scoreRace :: Race -> M.Map Sailor Points
+        scoreRace race = M.fromList
             [ (sailor, score)
-            | race <- races
-            , sailor <- S.toList allSailors
-            , let ranks :: M.Map Sailor Int
+            | let ranks :: M.Map Sailor Int
                   ranks = raceRanks race
-            , let score :: Points
-                  score
+            , sailor <- S.toList allSailors
+            , let score
                     | Just rank <- M.lookup sailor ranks
                     = Points $ realToFrac rank
                     | sailor `elem` raceRCs race
@@ -55,18 +49,19 @@ scoreSeries series = series'
                     | otherwise
                     = dnfRank
             ]
-          where
-            scores :: M.Map Sailor [Points]
-            scores = M.fromListWith (++)
-                [ (sailor, [Points $ realToFrac n])
-                | race <- races
-                , (sailor, n) <- M.toList $ raceRanks race
-                ]
-            avgScores :: M.Map Sailor Points
-            avgScores = fmap meanPoints scores
 
-            dnfRank :: Points
-            dnfRank = Points $ realToFrac $ succ $ maximum $ map (length . raceFinishers) races
+        scores :: M.Map Sailor [Points]
+        scores = M.fromListWith (++)
+            [ (sailor, [Points $ realToFrac n])
+            | race <- races
+            , (sailor, n) <- M.toList $ raceRanks race
+            ]
+
+        avgScores :: M.Map Sailor Points
+        avgScores = fmap meanPoints scores
+
+        dnfRank :: Points
+        dnfRank = Points $ realToFrac $ succ $ maximum $ map (length . raceFinishers) races
 
 meanPoints :: [Points] -> Points
 meanPoints = coerce (mean @Float)
--- Table.hs
+++ Table.hs
@@ -1,43 +1,65 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ParallelListComp #-}
 
 module Main where
 
 import Lucid
 import Results
+import Scoring
 
-import Data.List (intersperse)
+import Data.List (intersperse, sortBy)
+import Data.Ord (comparing)
+import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 import Data.Foldable
 import Data.Maybe
 
 main :: IO ()
 main = do
-    results <- readRaces "results/2023-01-08"
-    let resultsTable = rankingsTable results
-    renderToFile "results.html" resultsTable
+    series <- readSeries "results"
+    renderToFile "scores.html" $ doctypehtml_ $ do
+        header
+        body_ $ do
+            h1_ [class_ "title"] "Score Summary"
+            dayScoreTable (scoreSeries series)
 
-rankingsTable :: [Race] -> Html ()
-rankingsTable races = doctypehtml_ $ do
-    head_ $ do
-        link_ [rel_ "stylesheet", href_ "rankings.css"]
-        link_ [rel_ "stylesheet", href_ "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"]
-        script_ [src_ "rankings.js"] (return () :: Html ())
-    body_ $ do
-        table_ [classes_ ["table", "rankings"]] $ do
-            thead_ $ do
-                tr_ $ mapM_ (th_ . toHtml) $ ["Position"] ++ ["Race " ++ show i | (i, _) <- zip [1..] races]
-                tr_ $ do
-                    th_ "RC"
-                    mapM_ (td_ . ul_ . foldMap (li_ . sailor) . S.toList . raceRCs) races
+            h1_ [class_ "title"] "Per-race results"
+            mconcat
+                [ do h2_ [class_ "subtitle"] $ toHtml day
+                     rankingsTable
+                        $ M.fromList
+                          [ ("Race " ++ show i, race)
+                          | i <- [1..]
+                          | race <- races
+                          ]
+                | (day, races) <- M.toList series
+                ]
 
-            let mkRow :: Int -> [Maybe Sailor] -> Html ()
-                mkRow rank sailors = do
-                    th_ $ toHtml $ show rank
-                    mapM_ (maybe (return ()) (td_  . sailor)) sailors
-            tbody_
-                $ mapM_ tr_
-                $ zipWith mkRow [1..]
-                $ getRanking $ racesRankings races
+header :: Html ()
+header = head_ $ do
+    link_ [rel_ "stylesheet", href_ "rankings.css"]
+    link_ [rel_ "stylesheet", href_ "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"]
+    script_ [src_ "rankings.js"] (return () :: Html ())
+
+-- | Summarize rankings of a day's races.
+rankingsTable :: M.Map String Race -> Html ()
+rankingsTable races =
+    table_ [classes_ ["table", "is-striped", "is-hoverable", "rankings"]] $ do
+        thead_ $ do
+            tr_ $ mapM_ (th_ . toHtml) $ ["Position"] ++ M.keys races
+            tr_ $ do
+                th_ "RC"
+                mapM_ (td_ . ul_ . foldMap (li_ . sailor) . S.toList . raceRCs) races
+
+        let mkRow :: Int -> [Maybe Sailor] -> Html ()
+            mkRow rank sailors = do
+                th_ $ toHtml $ show rank
+                mapM_ (maybe (return ()) (td_  . sailor)) sailors
+        tbody_
+            $ mapM_ tr_
+            $ zipWith mkRow [1..]
+            $ getRanking $ racesRankings
+            $ M.elems races
 
 racesRankings :: [Race] -> Ranking [Maybe Sailor]
 racesRankings = 
@@ -50,3 +72,36 @@ sailor :: Sailor -> Html ()
 sailor s =
     span_ [class_ "sailor", data_ "sailor" (sailorName s)]
     $ toHtml $ sailorName s
+
+instance ToHtml Points where
+    toHtml (Points n) = toHtml $ show $ round n
+    toHtmlRaw = toHtml
+
+-- | Summary of sailors' per-day scores.
+dayScoreTable :: M.Map String [M.Map Sailor Points]
+              -> Html ()
+dayScoreTable results =
+    table_ [classes_ ["table", "is-striped", "is-hoverable", "rankings"]] $ do
+        thead_ $ do
+            tr_ $ mapM_ (th_ . toHtml) $ ["Day"] ++ map sailor allSailors
+        tbody_ $ do
+            forM_ (M.toList results) $ \(day, points) -> tr_ $ do
+                th_ $ toHtml day
+                let dayPoints = M.unionsWith (<>) points
+                mconcat
+                    [ td_ $ toHtml $ fromJust $ M.lookup sailor dayPoints
+                    | sailor <- allSailors
+                    ]
+            tfoot_ $ tr_ $ do
+                th_ "total"
+                sequenceA_
+                    [ td_ $ toHtml $ totals M.! sailor
+                    | sailor <- allSailors
+                    ]
+
+  where
+    allSailors :: [Sailor]
+    allSailors = map fst $ sortBy (comparing snd) $ M.toList totals
+
+    totals :: M.Map Sailor Points
+    totals = M.unionsWith (<>) $ concat $ M.elems results

e72eb4ace8679f5862a4ea045d6705ddf2c06659
Laura <dietz@cs.unh.edu>
2023-12-31 18:05:23 -0500

    upload frosty logo for og:image attribute

--- upload.sh
+++ upload.sh
@@ -6,4 +6,4 @@ KEY="${KEY:-~/.ssh/frosty}"
 cabal run results 2023-2024
 dest="frosty@smart-cactus.org:/var/www/frosty.smart-cactus.org"
 scp -i $KEY scores.html "$dest/index.html"
-scp -i $KEY rankings.js rankings.css "$dest"
+scp -i $KEY rankings.js rankings.css logo.gif "$dest"

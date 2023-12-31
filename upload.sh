8b45496bd2ba4285435ca3829a3b57cdc00aee01
Laura <dietz@cs.unh.edu>
2023-12-31 18:47:30 -0500

    meta-tags fix build

--- upload.sh
+++ upload.sh
@@ -4,7 +4,7 @@ set -e
 
 KEY="${KEY:-~/.ssh/frosty}"
 cabal run results 2023-2024
-wkhtmltoimage  --width 1200 --height 630 scores.html preview.png
+# wkhtmltoimage  --width 1200 --height 630 scores.html preview.png
 dest="frosty@smart-cactus.org:/var/www/frosty.smart-cactus.org"
 scp -i $KEY scores.html "$dest/index.html"
 scp -i $KEY rankings.js rankings.css preview.png "$dest"

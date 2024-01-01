3272818848ac6f430f34bdd87a7f57696b06f77d
Ben Gamari <ben@smart-cactus.org>
2023-12-31 19:17:00 -0500

    wkhtmltoimage: enable local file access

--- upload.sh
+++ upload.sh
@@ -7,7 +7,7 @@ sudo apt-get install wkhtmltopdf
 
 KEY="${KEY:-~/.ssh/frosty}"
 cabal run results 2023-2024
-wkhtmltoimage  --width 1200 --height 630 scores.html preview.png
+wkhtmltoimage --enable-local-file-access --width 1200 --height 630 scores.html preview.png
 dest="frosty@smart-cactus.org:/var/www/frosty.smart-cactus.org"
 scp -i $KEY scores.html "$dest/index.html"
 scp -i $KEY rankings.js rankings.css preview.png "$dest"
